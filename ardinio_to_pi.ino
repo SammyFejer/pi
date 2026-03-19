2b5dd35c68650d1b12ade3c25386ef14c649ae7a
SammyFejer <0348257@schoolsnet.act.edu.au>
2026-03-19 20:14:11 +1100

    Add files via upload

---
+++ ardinio_to_pi.ino
@@ -0,0 +1,20 @@
+char message[20];
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0){
+int data = 
+Serial.readBytesUntil('/n', message, sizeof(message)-1 );
+Serial.print(data += 1);
+
+
+    
+  }
+
+}
